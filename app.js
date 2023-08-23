815533c1714f34b50b523e528f563435718ac618
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-23 19:21:50 +0600

    Update app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import Header from './components/Header'
+  
+  const App = () => {
+      
+      return(
+      <div className='container'>
+      <Header/>
+      </div>
+      
+      
+      )
+  }
+
+
+export default App
+
+
+
+
